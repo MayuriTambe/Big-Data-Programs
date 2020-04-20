94331af89e4a6878876f3fd9c6c0ca91534840f3
MayuriTambe <tambemayurii190@gmail.com>
2020-04-20 13:01:53 -0400

    ADDED:To find users with lowest average hours

---
+++ HIVE/lowest_avghours.py
@@ -0,0 +1,21 @@
+import pyhive
+import pandas as pd
+from pyhive import hive
+import sys
+
+#Creating connection to hive server and connecting database
+connection = hive.Connection(host='localhost', port=10000, auth='NOSASL', database='users')
+
+#Loading database into hive
+userinfo = pd.read_sql('select * from userinfo', connection)
+
+#Converting to datetime format
+userinfo['userinfo.working_hours'] = pd.to_datetime(userinfo['userinfo.working_hours'])
+
+#Calaculating total working hours mean
+avg_hours = userinfo[userinfo['userinfo.working_hours'] < userinfo['userinfo.working_hours'].mean()]
+print(avg_hours)
+
+#Users with highest avghours hours
+highest_avghours_users=avg_hours['userinfo.user_name']
+print(highest_avghours_users)
